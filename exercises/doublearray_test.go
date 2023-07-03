b96e59630b02dee6781d27abb32599c060b88e3e
davidtaing <adavidtaing@gmail.com>
2023-07-03 18:32:41 +1000

    complete Beginner - Lost Without a Map
    https://www.codewars.com/kata/57f781872e3d8ca2a000007e/train/go

---
+++ exercises/doublearray_test.go
@@ -0,0 +1,25 @@
+package exercises
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestDoubleArray(t *testing.T) {
+	tests := []struct {
+		name  string
+		input []int
+		want  []int
+	}{
+		{name: "Double int[]{1, 2, 3}", input: []int{1, 2, 3}, want: []int{2, 4, 6}},
+		{name: "Double []int{10, 20, 30}", input: []int{10, 20, 30}, want: []int{20, 40, 60}},
+		{name: "Double []int{50, 100, 200}", input: []int{50, 100, 200}, want: []int{100, 200, 400}},
+	}
+
+	for _, tc := range tests {
+		got := DoubleArray(tc.input)
+		if !reflect.DeepEqual(got, tc.want) {
+			t.Errorf("%s: expected %v, got %v", tc.name, tc.want, got)
+		}
+	}
+}
