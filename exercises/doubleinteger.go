3844e1620f03cf91a7d89f55c403e40b8d5e962b
davidtaing <adavidtaing@gmail.com>
2023-07-03 17:57:41 +1000

    docs: add comment to You Can't Code Under Pressure

--- exercises/doubleinteger.go
+++ exercises/doubleinteger.go
@@ -1,3 +1,10 @@
+/**
+ * You Can't Code Under Pressure
+ *
+ * Double the integer and return it
+ * Source: https://www.codewars.com/kata/53ee5429ba190077850011d4/train/go
+ */
+
 package exercises
 
 func DoubleInteger(i int) int {
